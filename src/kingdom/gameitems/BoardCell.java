621e3a91775e92ddbef7757972d848fb3ee3dbd6
varunrayen <varunrayen@gmail.com>
2013-02-15 01:55:36 -0500

    changes files

--- src/kingdom/gameitems/BoardCell.java
+++ src/kingdom/gameitems/BoardCell.java
@@ -11,7 +11,7 @@ import javax.swing.JPanel;
 /**
  * Cell on border. The cell may be empty (item=null) or have some item on the sell (castle or tile). At the beginning of the game 
  * all cells are placed on the game border with empty item. During action (when user move tile to game border), the  cell where tile is placed
- * receive BoardItem. When paint() is called to repaing the cell, picture/tile/hint... may be shown. So the BorderCell becomes visible in 
+ * receive BoardItem. When paint() is called to repaint the cell, picture/tile/hint... may be shown. So the BorderCell becomes visible in 
  * the game border
  */
 public class BoardCell extends JPanel{
